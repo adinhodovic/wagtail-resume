e399f1d91c290daf4a4f6fb441190550758f2b62
Adin Hodovic <hodovicadin@gmail.com>
2019-12-25 14:19:38 +0100

    Cleanup files, directories and settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,23 +1,12 @@
-from pathlib import Path
-
 DEBUG = True
-USE_TZ = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "very-secret"
-
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite3"}}
 
 ROOT_URLCONF = "tests.urls"
 
 DJANGO_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.messages",
-    "django.contrib.sessions",
-    "django.contrib.sites",
-    "django.contrib.staticfiles",
 ]
 
 THIRD_PARTY_APPS = [
@@ -36,24 +25,11 @@ THIRD_PARTY_APPS = [
     "wagtailmetadata",
     "modelcluster",
     "taggit",
-    "django_extensions",
 ]
 
 LOCAL_APPS = ["wagtail_resume", "tests"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
-    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
-]
 
 TEMPLATES = [
     {
@@ -64,17 +40,11 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
             ]
         },
     }
 ]
 
 SITE_ID = 1
-STATIC_URL = "/static/"
-
-MEDIA_ROOT = Path(__file__).parent / "media"
-MEDIA_URL = "/media/"
 
 WAGTAIL_SITE_NAME = "My Example Site"

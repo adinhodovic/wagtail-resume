0c586bca5b4086d664b4bb6e54b7784dc09e649c
Adin Hodovic <hodovicadin@gmail.com>
2020-01-28 13:47:49 +0100

    Remove installed and 3rd party apps

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,3 @@
-DEBUG = True
-
 SECRET_KEY = "very-secret"
 
 ROOT_URLCONF = "tests.urls"
@@ -33,9 +31,7 @@ THIRD_PARTY_APPS = [
     "taggit",
 ]
 
-LOCAL_APPS = ["wagtail_resume", "tests"]
-
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = ["wagtail_resume", "tests"]
 
 TEMPLATES = [
     {

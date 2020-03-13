1f7a77878f400cf75f17e55bc9bd3466f1a97039
Adin Hodovic <hodovicadin@gmail.com>
2020-03-13 17:00:10 +0100

    Add a verbose name

--- wagtail_resume/apps.py
+++ wagtail_resume/apps.py
@@ -3,10 +3,4 @@ from django.apps import AppConfig
 
 class WagtailResumeConfig(AppConfig):
     name = "wagtail_resume"
-
-    def ready(self):
-        try:
-            # pylint: disable=unused-import,import-outside-toplevel
-            import wagtail_resume.signals
-        except ImportError:
-            pass
+    verbose_name = "Wagtail Resume"

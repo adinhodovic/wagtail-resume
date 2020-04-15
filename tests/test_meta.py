52fa8b576bb8e0493285a0c649a34d3e2133066f
Adin Hodovic <hodovicadin@gmail.com>
2020-04-15 12:28:04 +0200

    Adjust readme, add dev dependencies and add missing url test (#29)
    * Add readme
    
    * Use title

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -10,14 +10,22 @@ pytestmark = pytest.mark.django_db
 def test_meta(client):
     site = Site.objects.first()
     resume = CustomResumePage(
-        title="Resume", full_name="Adin Hodovic", role="Software engineer",
+        title="Resume",
+        full_name="Adin Hodovic",
+        role="Software engineer",
+        about="This is a test",
     )
     site.root_page.add_child(instance=resume)
     res = client.get(resume.full_url)
     soup = BeautifulSoup(res.content, "html.parser")
 
     description = soup.find("meta", attrs={"name": "description"})
-    assert description["content"] == "Adin Hodovic - Software engineer"
+    assert (
+        description["content"] == f"{resume.full_name} - {resume.role}. {resume.about}"
+    )
+
+    url = soup.find("meta", attrs={"itemprop": "url"})
+    assert url["content"] == resume.get_full_url()
 
     title = soup.find("title")
-    assert title.text == "Adin Hodovic"
+    assert title.text == resume.full_name

b70733cf47dba8202f481d7c520b5bd2f31467a7
siht <siht@users.noreply.github.com>
2020-04-19 13:29:46 -0500

    Fixup
    Fixup
    
    Compile mesasges pre test

---
+++ tests/test_localization.py
@@ -0,0 +1,24 @@
+import pytest
+from wagtail.core.models import Site
+
+from .models import CustomResumePage
+
+pytestmark = pytest.mark.django_db
+
+
+def test_translations(client):
+
+    site = Site.objects.first()
+    resume = CustomResumePage(
+        title="Resume", full_name="Adin Hodovic", role="Software engineer",
+    )
+    site.root_page.add_child(instance=resume)
+    content = client.get(resume.full_url, HTTP_ACCEPT_LANGUAGE="sv").content.decode(
+        "utf-8"
+    )
+
+    assert "Om" in content
+    assert "About" not in content
+
+    assert "Hämta PDF" in content
+    assert "Get PDF" not in content

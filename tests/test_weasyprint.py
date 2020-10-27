a65ca0add2644c51dffc76ba8d03796e70ce1f26
Adin Hodovic <hodovicadin@gmail.com>
2020-10-27 23:44:03 +0100

    Check if resume exists before generating (#40)
    * Check if resume exists before generating
    
    * FIx comment
    
    * WIP

--- tests/test_weasyprint.py
+++ tests/test_weasyprint.py
@@ -52,9 +52,21 @@ def test_weasyprint_with_no_page_id(client, mocker):
     # Test random page pdf generation
     url = f"{reverse('generate_resume_pdf')}"
     res = client.get(url)
-    print(res)
     assert b"Missing page id for resume generation" in res.content
     assert res.status_code == 400
+
+
+def test_weasyprint_no_resume(client, mocker):
+    mocker.patch("wagtail_resume.views.HTML")
+    site = Site.objects.first()
+    resume = CustomResumePage(
+        title="Resume", full_name="Adin Hodovic", role="Software engineer", font="lato"
+    )
+    site.root_page.add_child(instance=resume)
+    # Test non existent resume
+    url = f"{reverse('generate_resume_pdf')}?page_id=9999"
+    res = client.get(url)
+    assert res.status_code == 404
 
 
 def test_weasyprint_logger_warnings_disabled():
